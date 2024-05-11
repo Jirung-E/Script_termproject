2f38bb3afed95aabadf9b73bf1b4d4443104aa03
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-05-12 01:24:12 +0900

    전기차 충전소 찾기로 결정

---
+++ README.md
@@ -0,0 +1,6 @@
+# 전기차 충전소 찾기 앱
+https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15100485  
+
+- json
+- 지도
+- 그래프: 검색위치의 충전기 상태(충전가능, 충전중, 고장 등) 그래프로 표시
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
+import json
 import requests
-import xml.etree.ElementTree as ET
 import tkinter
 
 
-def make_url():
-    pass
-
 def get_data():
-    url = 'http://openapi.kepco.co.kr/service/EvInfoServiceV2'
-    service_key_encoding = "AAwo2DlYdyt1drOL9r%2BRcp2s6ROwcIQ2iYJs%2FbGgKLYS25uMWxpubIWzUj89JO1DwytB%2FirDqJkK0JvE4YQ5hw%3D%3D"
-    service_key_decoding = "AAwo2DlYdyt1drOL9r+Rcp2s6ROwcIQ2iYJs/bGgKLYS25uMWxpubIWzUj89JO1DwytB/irDqJkK0JvE4YQ5hw=="
-    queryParams = {'serviceKey': service_key_encoding}
+    with open('service_key.json', 'r') as f:
+        service_key = json.load(f)
+    
+    url = "https://api.odcloud.kr/api/EvInfoServiceV2/v1/getEvSearchList"
+    queryParams = {
+        "serviceKey": service_key["decoding"],
+        "cond[addr::LIKE]": "서울특별시",
+    }
 
-    response = requests.get(url, params=queryParams)
-    print(response.text)
+    return requests.get(url, params=queryParams).json()
 
 
-get_data()
+data = get_data()
+
+print(data)
+

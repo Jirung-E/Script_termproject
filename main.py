4e9caa4ff369b3c24211cc2a9db1222ed0b0b594
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-05-18 17:11:26 +0900

    gui - input, graph

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
 import requests
 from tkinter import *
+from tkinter.ttk import Progressbar
+
+from placeholder import *
 
 
 def get_location():
@@ -32,32 +35,75 @@ class GUI:
         self.window.title("전기차 충전소 검색")
         
         button_font = ("맑은 고딕", 24)
+        default_font = ("맑은 고딕", 20)
 
         interaction_frame = Frame(self.window)
         interaction_frame.pack(side=LEFT, fill=BOTH, expand=True)
-        
+
         map_frame = Frame(self.window, padx=10, pady=10)
         map_frame.pack(side=LEFT, fill=BOTH, expand=True)
 
         button_frame = Frame(interaction_frame, width=400, height=100, padx=10, pady=10)
         button_frame.pack(side=TOP)
 
-        self.search_button = Button(button_frame, text="검색", font=button_font, command=self.search)
+        self.search_button = Button(button_frame, text="검색", font=button_font, command=self.switch_to_search_mode)
         self.search_button.place(width=90, height=90)
 
-        self.favorites_button = Button(button_frame, text="즐겨\n찾기", font=button_font, command=self.search)
+        self.favorites_button = Button(button_frame, text="즐겨\n찾기", font=button_font, command=self.switch_to_favorites_mode)
         self.favorites_button.place(x=100, width=90, height=90)
 
-        self.recent_button = Button(button_frame, text="최근\n기록", font=button_font, command=self.search)
+        self.recent_button = Button(button_frame, text="최근\n기록", font=button_font, command=self.switch_to_recent_mode)
         self.recent_button.place(x=200, width=90, height=90)
 
-        self.share_button = Button(button_frame, text="공유", font=button_font, command=self.search)
+        self.share_button = Button(button_frame, text="공유", font=button_font, command=self.share)
         self.share_button.place(x=300, width=90, height=90)
+
+        self.search_input = PlaceholderText(interaction_frame, placeholder="주소를 입력하세요", font=default_font)
+        self.search_input.place(x=10, y=110, width=390, height=90)
+
+        self.add_to_favorites_button = Button(interaction_frame, text="즐겨찾기에 추가", font=default_font, command=self.search)
+        self.add_to_favorites_button.place(x=10, y=210, width=390, height=50)
+
+        info_frame = Frame(interaction_frame, width=400, height=330)
+        info_frame.pack(side=BOTTOM)
+
+        self.available_label = Label(info_frame, text="사용가능", font=default_font)
+        self.available_label.place(x=10, y=170)
+        self.progress = Progressbar(info_frame, orient=HORIZONTAL, length=180, mode='determinate', maximum=100, value=10)
+        self.progress.place(x=130, y=175, height=40)
+        self.available_count_label = Label(info_frame, text="123개", font=default_font)
+        self.available_count_label.place(x=320, y=170)
+
+        self.occupied_label = Label(info_frame, text="사용중", font=default_font)
+        self.occupied_label.place(x=10, y=220)
+        self.progress = Progressbar(info_frame, orient=HORIZONTAL, length=180, mode='determinate', maximum=100, value=10)
+        self.progress.place(x=130, y=225, height=40)
+        self.occupied_count_label = Label(info_frame, text="70개", font=default_font)
+        self.occupied_count_label.place(x=320, y=220)
+
+        self.disabled_label = Label(info_frame, text="사용불가", font=default_font)
+        self.disabled_label.place(x=10, y=270)
+        self.progress = Progressbar(info_frame, orient=HORIZONTAL, length=180, mode='determinate', maximum=100, value=10)
+        self.progress.place(x=130, y=275, height=40)
+        self.disabled_count_label = Label(info_frame, text="2개", font=default_font)
+        self.disabled_count_label.place(x=320, y=270)
 
         self.map = Canvas(map_frame, width=660, height=660, bg="white")
         self.map.pack()
 
         self.window.mainloop()
+
+    def switch_to_search_mode(self):
+        pass
+
+    def switch_to_favorites_mode(self):
+        pass
+
+    def switch_to_recent_mode(self):
+        pass
+
+    def share(self):
+        pass
 
     def search(self):
         pass
---
+++ placeholder.py
@@ -0,0 +1,53 @@
+from tkinter import Entry, Text
+
+
+class PlaceholderEntry(Entry):
+    def __init__(self, master=None, placeholder="PLACEHOLDER", color='grey', *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.placeholder = placeholder
+        self.placeholder_color = color
+        self.default_fg_color = self['fg']
+
+        self.bind("<FocusIn>", self.foc_in)
+        self.bind("<FocusOut>", self.foc_out)
+
+        self.put_placeholder()
+
+    def put_placeholder(self):
+        self.insert(0, self.placeholder)
+        self['fg'] = self.placeholder_color
+
+    def foc_in(self, *args):
+        if self['fg'] == self.placeholder_color:
+            self.delete('0', 'end')
+            self['fg'] = self.default_fg_color
+
+    def foc_out(self, *args):
+        if not self.get():
+            self.put_placeholder()
+
+
+class PlaceholderText(Text):
+    def __init__(self, master=None, placeholder="PLACEHOLDER", color='grey', *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.placeholder = placeholder
+        self.placeholder_color = color
+        self.default_fg_color = self['fg']
+
+        self.bind("<FocusIn>", self.foc_in)
+        self.bind("<FocusOut>", self.foc_out)
+
+        self.put_placeholder()
+
+    def put_placeholder(self):
+        self.insert(1.0, self.placeholder)
+        self['fg'] = self.placeholder_color
+
+    def foc_in(self, *args):
+        if self['fg'] == self.placeholder_color:
+            self.delete('1.0', 'end')
+            self['fg'] = self.default_fg_color
+
+    def foc_out(self, *args):
+        if not self.get('1.0', 'end-1c'):
+            self.put_placeholder()
